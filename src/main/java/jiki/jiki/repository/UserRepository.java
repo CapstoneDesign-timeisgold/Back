cc78278b9ac7f8b160c51b5f329664a734ef75c2
unfl1 <flxmaut10@gmail.com>
2024-05-21 00:45:53 +0900

    dto 주석추가

--- src/main/java/jiki/jiki/repository/UserRepository.java
+++ src/main/java/jiki/jiki/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-    Optional<SiteUser> findByUsername(String username);
+    Optional<SiteUser> findByUsername(String username); //아이디로 사용자 찾기
     Optional<SiteUser> findByNickname(String nickname); // 닉네임으로 사용자 찾기
 }
 
