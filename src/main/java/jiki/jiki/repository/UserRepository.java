b18c24cd554e46e831ee0ce951deac7522b7798a
unfl1 <flxmaut10@gmail.com>
2024-05-21 00:47:10 +0900

    레포지토리 변경1

--- src/main/java/jiki/jiki/repository/UserRepository.java
+++ src/main/java/jiki/jiki/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-    Optional<SiteUser> findByUsername(String username); //아이디로 사용자 찾기
+    Optional<SiteUser> findByUsername(String username); //사용자 아이디로 사용자 찾기
     Optional<SiteUser> findByNickname(String nickname); // 닉네임으로 사용자 찾기
 }
 
