08b3c7e040d1ebea11dfcd0c076c7d65b3b5ff1c
unfl1 <flxmaut10@gmail.com>
2024-05-20 13:38:29 +0900

    cors 전체 허용

--- src/main/java/jiki/jiki/config/WebMvcConfig.java
+++ src/main/java/jiki/jiki/config/WebMvcConfig.java
@@ -14,9 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedOriginPatterns("*") // 모든 도메인 허용
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
