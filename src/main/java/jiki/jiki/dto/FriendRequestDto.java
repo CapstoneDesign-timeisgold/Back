e5e41dd731705737a559050fcc92e22166841a15
unfl1 <flxmaut10@gmail.com>
2024-06-27 00:00:20 +0900

    친구목록 수정1

--- src/main/java/jiki/jiki/dto/FriendRequestDto.java
+++ src/main/java/jiki/jiki/dto/FriendRequestDto.java
@@ -3,7 +3,8 @@ package jiki.jiki.dto;
 import lombok.Data;
 
 @Data
-public class FriendRequestDto {
+public class
+FriendRequestDto {
     private String username; // 요청 보내는 사람 username
     private String username2; // 요청 받는 사람 username
 }
